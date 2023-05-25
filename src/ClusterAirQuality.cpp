b9bf3ba8d66c29e0fea789200cfd9f0725eae104
Jeff Culbert <jeff.culbert@gmail.com>
2023-05-25 11:15:04 -0400

    Airquality first pass

--- src/ClusterAirQuality.cpp
+++ src/ClusterAirQuality.cpp
@@ -32,12 +32,10 @@ namespace cluster_lib
 {
 static ClusterAirQuality *clusterWorker;
 
-static const unsigned INT_NUM = 3; // Note, valid values depends on pin number (see GPIO_ExtIntConfig)
+static const unsigned INT_NUM = 13; // Note, valid values depends on pin number (see GPIO_ExtIntConfig)
 
 static void int_callback(unsigned char intNo)
 {
-    GPIO_IntDisable(1<<INT_NUM);
-    GPIO_IntClear(1<<INT_NUM);
     clusterWorker->RequestProcess(0);
 }
 
@@ -51,30 +49,40 @@ ClusterAirQuality::ClusterAirQuality (uint32_t endpoint, PostEventCallback _post
     : ClusterWorker(endpoint, _postEventCallback), airquality(0)
 {
     clusterWorker = this;
-    GPIO_ExtIntConfig(SL_EMLIB_GPIO_INIT_AIRQUALITY_INT_PORT, SL_EMLIB_GPIO_INIT_AIRQUALITY_INT_PIN, INT_NUM, 0, 1, true); // falling edge only, interrupt enabled
+    GPIO_IntDisable(1<<INT_NUM);
+    GPIO_ExtIntConfig(SL_EMLIB_GPIO_INIT_AIRQUALITY_INT_PORT, SL_EMLIB_GPIO_INIT_AIRQUALITY_INT_PIN, INT_NUM, 0, 1, false); // falling edge only, interrupt disabled
     GPIOINT_CallbackRegister(INT_NUM, int_callback);
 
     status = sl_ccs811_init(sl_i2cspm_sensor);
+    EFR32_LOG("sl_ccs811_init %d", status);
     if (status != SL_STATUS_OK)
         return; // For checking in debugger
-    status = sl_ccs811_set_measure_mode(sl_i2cspm_sensor, CCS811_MEASURE_MODE_DRIVE_MODE_10SEC | CCS811_MEASURE_MODE_INTERRUPT);
+    status = sl_ccs811_set_measure_mode(sl_i2cspm_sensor, CCS811_MEASURE_MODE_DRIVE_MODE_60SEC | CCS811_MEASURE_MODE_INTERRUPT);
+    EFR32_LOG("set_measure_mode %d", status);
     if (status != SL_STATUS_OK)
         return; // For checking in debugger
 
+    GPIO_IntEnable(1<<INT_NUM);
 }
 
 ClusterAirQuality::~ClusterAirQuality ()
 {
 }
 
+static uint8_t state;
 void ClusterAirQuality::Process()
 {
-    //if ( !sl_ccs811_is_data_available(sl_i2cspm_sensor) )
-    //{
+    status = sl_ccs811_get_status(sl_i2cspm_sensor, &state);
+
+    if ( !sl_ccs811_is_data_available(sl_i2cspm_sensor) )
+    {
         // Check again after a delay
-        //RequestProcess(1000);
-        //return;
-    //}
+        // This fixes the problem where an interrupt occurs immediately after initializing the chip
+        // but data is not ready at this point.
+        // Things work correctly after the first reading completes, so we don't get to this code.
+        RequestProcess(1000);
+        return;
+    }
 
     uint16_t eco2, tvoc;
     sl_ccs811_get_measurement(sl_i2cspm_sensor, &eco2, &tvoc);
@@ -84,7 +92,9 @@ void ClusterAirQuality::Process()
     airquality = 10*tvoc;
     chip::DeviceLayer::PlatformMgr().ScheduleWork(UpdateClusterState, reinterpret_cast<intptr_t>(nullptr));
 
-    GPIO_IntEnable(1<<INT_NUM);
+    // For safety we request process with delay greater than measure period.
+    // This should be always superceded be the measurement complete interrupt
+    RequestProcess(90*1000);
 }
 
 } /* namespace cluster_lib */
